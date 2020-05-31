c08d3c4e865df5cd26862fd55881c6074b80a610
mrugankray <mrugankray2@gmail.com>
2020-06-01 03:15:04 +0530

    added / after register route in django_crud/urls.py

--- django_crud/urls.py
+++ django_crud/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('register', include('users.urls')),
+    path('register/', include('users.urls')),
     path('profile', user_views.profile, name='profile'),
     path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
